7fd471be8dfc77135ccba7a6696b64e56b6cd56c
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-19 16:38:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,12 @@
 # }
 
 def select_winner(passengers)
-
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
 end
 
   # add the code snippet here!
